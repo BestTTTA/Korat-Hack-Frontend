90d5920935489a566a010662bf1fead7b3b8abe9
BestTTTA <bst12546@gmail.com>
2024-09-13 06:07:27 +0700

    add domain for image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['119.59.102.60','localhost'], 
+      domains: ['119.59.102.60','localhost', "korat-hack.vercel.app"], 
     },
   };
   
